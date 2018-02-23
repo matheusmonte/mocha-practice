43f53c0483ca3bb203c76925710dec34f688d3ef
Matheus Clark <msn@certi.org.br>
2018-02-23 07:28:48 -0400

    Remove unecessary var to solve issue #1
    Signed-off-by: Matheus Clark <msn@certi.org.br>

--- app/converter.js
+++ app/converter.js
@@ -1,7 +1,7 @@
 exports.rgbToHex = function(red, green, blue){
-    var redHex = red.toString(16);
-    var greenHex = green.toString(16);
-    var blueHex = blue.toString(16);
+    let redHex = red.toString(16);
+    let greenHex = green.toString(16);
+    let blueHex = blue.toString(16);
 
     return pad(redHex) + pad(greenHex) + pad(blueHex);
 };
@@ -12,9 +12,9 @@ function pad(hex){
 
 exports.hexToRgb = function(hex) {
 
-    var red   = parseInt(hex.substring(0, 2), 16);
-    var green = parseInt(hex.substring(2, 4), 16);
-    var blue  = parseInt(hex.substring(4, 6), 16);
+    let red   = parseInt(hex.substring(0, 2), 16);
+    let green = parseInt(hex.substring(2, 4), 16);
+    let blue  = parseInt(hex.substring(4, 6), 16);
   
     return [red, green, blue];
   
--- test/converter.js
+++ test/converter.js
@@ -4,9 +4,9 @@ var converter = require("../app/converter");
 describe("Color Code Converter", function(){
     describe("RGB to Hex conversion", function(){
         it("converts the basic colors", function(){
-            var redHex   = converter.rgbToHex(255, 0, 0);
-            var greenHex = converter.rgbToHex(0, 255, 0);
-            var blueHex  = converter.rgbToHex(0, 0, 255);
+            let redHex   = converter.rgbToHex(255, 0, 0);
+            let greenHex = converter.rgbToHex(0, 255, 0);
+            let blueHex  = converter.rgbToHex(0, 0, 255);
       
             expect(redHex).to.equal("ff0000");
             expect(greenHex).to.equal("00ff00");
@@ -15,9 +15,9 @@ describe("Color Code Converter", function(){
     });
     describe("Hex to RGB conversion", function(){
         it("converts the basic colors", function(){
-            var red   = converter.hexToRgb("ff0000");
-            var green = converter.hexToRgb("00ff00");
-            var blue  = converter.hexToRgb("0000ff");
+            let red   = converter.hexToRgb("ff0000");
+            let green = converter.hexToRgb("00ff00");
+            let blue  = converter.hexToRgb("0000ff");
       
             expect(red).to.deep.equal([255, 0, 0]);
             expect(green).to.deep.equal([0, 255, 0]);
